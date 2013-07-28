d1407e96b9fcba37dea66167b261a5566bc2030c
Brad Osgood <bosgood@gmail.com>
2013-07-28 19:52:20 -0400

    An attempt at a simple setup script...

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='repoman',
+    version='0.1',
+    description='Repository Update Manager',
+    author='Brad Osgood',
+    author_email='bosgood@gmail.com',
+    zip_safe=False,
+    include_package_data=True,
+    url='http://github.com/bosgood/repoman',
+    install_requires=[
+        'pyyaml==1.0.0'
+    ],
+    dependency_links=[],
+    platforms=["any"],
+)
